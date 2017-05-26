0d5d93f5735ca06597237efb352ec392a389468c
charleneclee <cclee@google.com>
2017-05-26 13:08:42 -0700

    finished events

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
 
+function getIt () {
+  alert("Hey!");
+}
+
+function frameIt () {
+  $(this).addclass(tasty);
+  $(this).css('border', '3m solid red');
+}
+
+function pressIt (n) {
+  if(n.which == 71) {
+    alert ("You have pressed the G key");
+  }
+}
+
+function submitIt() {
+  alert("Your form is going to be submitted now");
+  return;
+}
+
 $(document).ready(function(){
 
-// call functions here
+$('p').click(getIt ())
+
+$('img').load(frameIt())
+
+$('form').keydown(pressIt(n))
+
+$('form').submit(submitIt())
 
 });
